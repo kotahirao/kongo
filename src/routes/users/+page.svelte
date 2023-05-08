4118288312cea0021ea2662bd7c6cdce138d369d
平尾航太 <hiraokota@hiraowashinoAir.AirPort>
2023-05-09 00:17:47 +0900

    add button test

---
+++ src/routes/users/+page.svelte
@@ -0,0 +1,13 @@
+<div>
+  <Button on:click={() => clicked++} variant="raised">
+    <Label>クリック！</Label>
+  </Button>
+</div>
+
+<pre>クリック回数： {clicked}</pre>
+
+<script lang="ts">
+  import Button, {Label} from '@smui/button';
+
+  let clicked = 0;
+</script>
