2b7a11943472241518319141c8940a2c93d9bae0
平尾航太 <hiraokota@hiraowatarufutoshinoMacBook-Air.local>
2023-05-10 23:15:25 +0900

    add openapi generator

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -6,7 +6,10 @@
 
 <pre>クリック回数： {clicked}</pre>
 
+<Test></Test>
+
 <script lang="ts">
+	import Test from '$lib/components/Test.svelte';
   import Button, {Label} from '@smui/button';
 
   let clicked = 0;
