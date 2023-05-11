d0700cd05d80af76943b0390c236b0fa96924400
平尾航太 <hiraokota@hiraowatarufutoshinoMacBook-Air.local>
2023-05-12 00:03:24 +0900

    wip: user table

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -1,16 +1,99 @@
 <script lang="ts">
 	import Test from '$lib/components/Test.svelte';
-	import Button, { Label } from '@smui/button';
+	import type { PageData } from './$types';
+	import DataTable, {
+    Head,
+    Body,
+    Row,
+    Cell,
+    Pagination,
+  } from '@smui/data-table';
+  import Select, { Option } from '@smui/select';
+  import IconButton from '@smui/icon-button';
+  import { Label } from '@smui/common';
+
+  let rowsPerPage = 10;
+  let currentPage = 0;
+
+  $: start = currentPage * rowsPerPage;
+  $: end = Math.min(start + rowsPerPage, data.length);
+  $: slice = data.slice(start, end);
+  $: lastPage = Math.max(Math.ceil(data.length / rowsPerPage) - 1, 0);
+  $: if (currentPage > lastPage) {
+    currentPage = lastPage;
+  }
 
 	let clicked = 0;
+	export let data: PageData;
+	console.log(data);
 </script>
 
-<div>
+<!-- <div>
 	<Button on:click={() => clicked++} variant="raised">
 		<Label>クリック！</Label>
 	</Button>
-</div>
+</div> -->
 
 <pre>クリック回数： {clicked}</pre>
 
 <Test />
+
+<DataTable table$aria-label="Todo list" style="width: 100%;">
+  <Head>
+    <Row>
+      <Cell numeric>ID</Cell>
+      <Cell numeric>ユーザー名</Cell>
+    </Row>
+  </Head>
+  <Body>
+    {#each slice as user (user.id)}
+      <Row>
+        <Cell numeric>{user.id}</Cell>
+        <Cell>{user.name}</Cell>
+      </Row>
+    {/each}
+  </Body>
+
+  <Pagination slot="paginate">
+    <svelte:fragment slot="rowsPerPage">
+      <Label>Rows Per Page</Label>
+      <Select variant="outlined" bind:value={rowsPerPage} noLabel>
+        <Option value={10}>10</Option>
+        <Option value={25}>25</Option>
+        <Option value={100}>100</Option>
+      </Select>
+    </svelte:fragment>
+    <svelte:fragment slot="total">
+      {start + 1}-{end} of {data.length}
+    </svelte:fragment>
+
+    <IconButton
+      class="material-icons"
+      action="first-page"
+      title="First page"
+      on:click={() => (currentPage = 0)}
+      disabled={currentPage === 0}>first_page</IconButton
+    >
+    <IconButton
+      class="material-icons"
+      action="prev-page"
+      title="Prev page"
+      on:click={() => currentPage--}
+      disabled={currentPage === 0}>chevron_left</IconButton
+    >
+    <IconButton
+      class="material-icons"
+      action="next-page"
+      title="Next page"
+      on:click={() => currentPage++}
+      disabled={currentPage === lastPage}>chevron_right</IconButton
+    >
+    <IconButton
+      class="material-icons"
+      action="last-page"
+      title="Last page"
+      on:click={() => (currentPage = lastPage)}
+      disabled={currentPage === lastPage}>last_page</IconButton
+    >
+  </Pagination>
+</DataTable>
