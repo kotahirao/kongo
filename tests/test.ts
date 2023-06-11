457b232f275b8b9fd639e55bf959c42731e3ef64
平尾航太 <hiraokota@hiraowatarufutoshinoMacBook-Air.local>
2023-06-11 17:04:00 +0900

    簡単なplaywrightテスト実装

--- tests/test.ts
+++ tests/test.ts
@@ -2,8 +2,17 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
 	await page.goto('http://127.0.0.1:5173/users');
+
+	await expect(page).toHaveTitle('ユーザー一覧');
+
+	const createUserName = 'testuser';
+
 	await page.getByRole('button', { name: '新規登録' }).click();
-	await page.getByPlaceholder(' ').fill('testuser2');
-	await page.getByRole('button', { name: '新規登録' }).nth(1).click();
-	await page.getByRole('cell', { name: 'testuser2' }).click();
+	await page.getByPlaceholder(' ').click();
+	await page.getByPlaceholder(' ').fill(createUserName);
+	await page.getByRole('button', { name: '登録', exact: true }).click();
+	await page.getByRole('button', { name: 'last_page' }).click();
+	await page.getByRole('cell', { name: createUserName, exact: true }).isVisible;
+
+	await page.screenshot({ path: 'example.png' });
 });
