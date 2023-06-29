5d37f4fc97c4d80fc8ddab7c7ba502e833c9b87e
kota.hirao <kota.hirao@monocrea.co.jp>
2023-06-30 00:11:38 +0900

    add test describe

--- tests/test.ts
+++ tests/test.ts
@@ -1,15 +1,31 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-	await page.goto('http://127.0.0.1:5173/users');
+test.describe('ユーザー一覧', () => {
+	test('ユーザー登録', async ({ page }) => {
+		await page.goto('http://127.0.0.1:5173/users');
 
-	await expect(page).toHaveTitle('ユーザー一覧');
+		await expect(page).toHaveTitle('ユーザー一覧');
 
-	const createUserName = 'testuser';
+		const createUserName = 'testuser';
 
-	await page.getByRole('button', { name: '新規登録' }).click();
-	await page.getByPlaceholder(' ').click();
-	await page.getByPlaceholder(' ').fill(createUserName);
-	await page.getByRole('button', { name: '登録', exact: true }).click();
-	await page.getByRole('cell', { name: createUserName, exact: true }).isVisible;
+		await page.getByRole('button', { name: '新規登録' }).click();
+		await page.getByPlaceholder(' ').click();
+		await page.getByPlaceholder(' ').fill(createUserName);
+		await page.getByRole('button', { name: '登録', exact: true }).click();
+		await page.getByRole('cell', { name: createUserName, exact: true }).isVisible;
+	});
+
+	test('ユーザー登録2', async ({ page }) => {
+		await page.goto('http://127.0.0.1:5173/users');
+
+		await expect(page).toHaveTitle('ユーザー一覧');
+
+		const createUserName = 'testuser';
+
+		await page.getByRole('button', { name: '新規登録' }).click();
+		await page.getByPlaceholder(' ').click();
+		await page.getByPlaceholder(' ').fill(createUserName);
+		await page.getByRole('button', { name: '登録', exact: true }).click();
+		await page.getByRole('cell', { name: createUserName, exact: true }).isVisible;
+	});
 });
