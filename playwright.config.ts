e09b0d87fb85ad554db904df0209e36ae7a19f01
kota.hirao <kota.hirao@monocrea.co.jp>
2023-07-19 22:20:24 +0900

    use url in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,10 @@ import { devices, type PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'yarn dev',
-		port: 5173
+		url: 'http://127.0.0.1:5173/'
+	},
+	use: {
+		baseURL: 'http://127.0.0.1:5173/'
 	},
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
